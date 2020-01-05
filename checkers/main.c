1170d1c2990a668f4b0149ad49a391e449b41e81
tomluxton <thomas.luxton@yahoo.com.au>
2020-01-05 18:45:24 +1000

    Initial commit with initial visual board

---
+++ checkers/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("_________________\n");
+    printf("|_|o|_|o|_|o|_|o|\n");
+    printf("|o|_|o|_|o|_|o|_|\n");
+    printf("|_|o|_|o|_|o|_|o|\n");
+    printf("|_|_|_|_|_|_|_|_|\n");
+    printf("|_|_|_|_|_|_|_|_|\n");
+    printf("|x|_|x|_|x|_|x|_|\n");
+    printf("|_|x|_|x|_|x|_|x|\n");
+    printf("|x|_|x|_|x|_|x|_|\n");
+    printf("_________________\n");
+    return 0;
+}
