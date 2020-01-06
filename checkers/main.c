1eefe0f0c71e6b047394abe036c74145a8a5557c
tomluxton <thomas.luxton@yahoo.com.au>
2020-01-06 22:56:01 +1000

    updated initial display

--- checkers/main.c
+++ checkers/main.c
@@ -3,7 +3,20 @@
 
 int main()
 {
-    printf("_________________\n");
+    char board[17][10];
+
+    for (int i = 0; i < sizeof(board)/sizeof(board[0]); i++) {
+        board[i][0] = '_';
+    }
+
+    for (int j = 0; j < sizeof(board)/sizeof(board[0]); j++) {
+        printf("%c", board[j][0]);
+    }
+    printf("\n");
+
+    // rest of board can be fitted into the for loop with
+    // another layer of for loop O(n^2), with if statements
+
     printf("|_|o|_|o|_|o|_|o|\n");
     printf("|o|_|o|_|o|_|o|_|\n");
     printf("|_|o|_|o|_|o|_|o|\n");
