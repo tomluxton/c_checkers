aedc7307167f2508f69380d52bd4ccf1799ac343
tomluxton <thomas.luxton@yahoo.com.au>
2020-01-07 23:29:58 +1000

    Updated for loop and
    Simply put the two for loops into 1 and restructuring repo

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char board[17][10];
+
+    for (int i = 0; i < sizeof(board)/sizeof(board[0]); i++) {
+        board[i][0] = '_';
+        printf("%c", board[i][0]);
+    }
+
+
+    printf("\n");
+
+    // rest of board can be fitted into the for loop with
+    // another layer of for loop O(n^2), with if statements
+
+    printf("|_|o|_|o|_|o|_|o|\n");
+    printf("|o|_|o|_|o|_|o|_|\n");
+    printf("|_|o|_|o|_|o|_|o|\n");
+    printf("|_|_|_|_|_|_|_|_|\n");
+    printf("|_|_|_|_|_|_|_|_|\n");
+    printf("|x|_|x|_|x|_|x|_|\n");
+    printf("|_|x|_|x|_|x|_|x|\n");
+    printf("|x|_|x|_|x|_|x|_|\n");
+    printf("_________________\n");
+    return 0;
+}
